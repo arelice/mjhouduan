fcf521ade18f5953fb054794e926c7c59088a64c
arelice <141432992+arelice@users.noreply.github.com>
2024-07-03 22:40:41 +0800

    Update permission.ts

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -2,39 +2,16 @@ import type { Router } from 'vue-router'
 import { useAuthStoreWithout } from '@/store/modules/auth'
 
 export function setupPageGuard(router: Router) {
-  router.beforeEach(async (to, from, next) => {
+  router.beforeEach(async (to, next) => {
     const authStore = useAuthStoreWithout()
     if (!authStore.session) {
       try {
-        const data = await authStore.getSession()
-        if (String(data.auth) === 'false' && authStore.token)
-          authStore.removeToken()
-        if (to.path === '/500')
-          next({ name: 'Root' })
-        else
-          next()
+        await authStore.getSession()
       }
       catch (error) {
         console.error('Error fetching session:', error)
-        if (to.path !== '/500')
-          next({ name: '500' })
-        else
-          next()
       }
     }
-    else {
-      if (to.matched.some(record => record.meta.requiresAuth)) {
-        if (!authStore.token) {
-          next({
-            path: '/login',
-            query: { redirect: to.fullPath }
-          })
-        } else {
-          next()
-        }
-      } else {
-        next()
-      }
-    }
+    next()
   })
 }
