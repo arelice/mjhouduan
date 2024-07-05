b615c08c61225818d92246833dde18663d5ecc96
arelice <141432992+arelice@users.noreply.github.com>
2024-07-05 23:23:59 +0800

    Update aiMobileMenu.vue

--- src/views/mj/aiMobileMenu.vue
+++ src/views/mj/aiMobileMenu.vue
@@ -1,62 +1,78 @@
 <script setup lang="ts">
 import { SvgIcon } from '@/components/common';
 import { homeStore } from '@/store'
-import { computed,watch ,ref  } from 'vue'
+import { computed, watch, ref } from 'vue'
 import { router } from '@/router'
-
 import aiDrawInput from './aiDrawInput.vue'; 
-import {NDrawerContent,NDrawer} from "naive-ui";
+import OpenAISettings from './OpenAISettings.vue'; // 确保路径正确
+import { NDrawerContent, NDrawer } from "naive-ui";
 import { isDisableMenu } from '@/api';
-const st= ref({show:true})
 
-const goHome =computed(  () => {
-  //router.push('/')
-  return router.currentRoute.value.name
+const st = ref({ show: true });
+const showSettings = ref(false); // 控制 OpenAISettings 组件的显示
+
+const goHome = computed(() => {
+  return router.currentRoute.value.name;
 });
-function drawSent(e:any){
-  st.value.show=false;
-  //$emit('drawSent', e)
-  homeStore.setMyData({act:'draw',actData:e});
+
+function drawSent(e: any) {
+  st.value.show = false;
+  homeStore.setMyData({ act: 'draw', actData: e });
 }
 
-watch(()=>homeStore.myData.act, (n:string)=>{
-    if('showChat'==n){
-        router.push('/chat')
-    }
-    if('showDraw'==n){
-        router.push('/draw')
-        st.value.show=true;
-    }
-    if(n=='draw'){
-       st.value.show=false;
-    }
+watch(() => homeStore.myData.act, (n: string) => {
+  if ('showChat' == n) {
+    router.push('/chat');
+  }
+  if ('showDraw' == n) {
+    router.push('/draw');
+    st.value.show = true;
+  }
+  if (n == 'draw') {
+    st.value.show = false;
+  }
 });
+
+function toggleSettings() {
+  showSettings.value = !showSettings.value;
+}
 </script>
+
 <template>
-  <div class=" bg-gray-100 dark:bg-[#282832] h-[55px] flex  justify-around  items-center dark:text-white/70 " >
-      <div class="flex items-center justify-center flex-col"  @click="homeStore.setMyData({act:'showChat'}) "   :class="[ goHome =='Chat' ? 'active' : '']" >
-        <SvgIcon icon="ri:wechat-line" class="text-3xl"></SvgIcon>
-        <div class="text-[13px]">{{$t('mjtab.chat')}}</div>
-      </div>
-      <div  v-if="!isDisableMenu ( 'gpts')"  class="flex items-center justify-center flex-col "  @click="homeStore.setMyData({act:'showgpts'}) " >
-        <SvgIcon icon="ri:apps-fill" class="text-3xl"></SvgIcon>
-        <div class="text-[13px]">GPTs</div>
-      </div>
-
-
-      <div v-if="!isDisableMenu ( 'draws')" class="flex items-center justify-center flex-col "  @click="homeStore.setMyData({act:'showDraw'}) " :class="[goHome=='draw' ? 'active' : '']" >
-        <SvgIcon icon="ic:outline-palette" class="text-3xl"></SvgIcon>
-        <div class="text-[13px]">{{$t('mjtab.draw')}}</div>
-      </div>
-      <div  v-if="!isDisableMenu ( 'gallery')"  class="flex items-center justify-center flex-col " @click="homeStore.setMyData({act:'gallery'})" >
-        <SvgIcon icon="material-symbols:imagesmode-outline" class="text-3xl"></SvgIcon>
-        <div class="text-[13px]">{{$t('mjtab.gallery')}}</div>
-      </div> 
+  <div class="bg-gray-100 dark:bg-[#282832] h-[55px] flex justify-around items-center dark:text-white/70">
+    <div class="flex items-center justify-center flex-col" @click="homeStore.setMyData({ act: 'showChat' })" :class="[goHome == 'Chat' ? 'active' : '']">
+      <SvgIcon icon="ri:wechat-line" class="text-3xl"></SvgIcon>
+      <div class="text-[13px]">{{ $t('mjtab.chat') }}</div>
+    </div>
+    <div v-if="!isDisableMenu('gpts')" class="flex items-center justify-center flex-col" @click="homeStore.setMyData({ act: 'showgpts' })">
+      <SvgIcon icon="ri:apps-fill" class="text-3xl"></SvgIcon>
+      <div class="text-[13px]">GPTs</div>
+    </div>
+    <a :href="`https://api2.raojialong.space/login`" class="flex items-center justify-center flex-col">
+      <SvgIcon icon="whh:spiderman" class="text-3xl"></SvgIcon>
+      <div class="text-[13px]">控制台</div>
+    </a>
+    <div v-if="!isDisableMenu('draws')" class="flex items-center justify-center flex-col" @click="homeStore.setMyData({ act: 'showDraw' })" :class="[goHome == 'draw' ? 'active' : '']">
+      <SvgIcon icon="ic:outline-palette" class="text-3xl"></SvgIcon>
+      <div class="text-[13px]">{{ $t('mjtab.draw') }}</div>
+    </div>
+    <div v-if="!isDisableMenu('gallery')" class="flex items-center justify-center flex-col" @click="homeStore.setMyData({ act: 'gallery' })">
+      <SvgIcon icon="material-symbols:imagesmode-outline" class="text-3xl"></SvgIcon>
+      <div class="text-[13px]">{{ $t('mjtab.gallery') }}</div>
+    </div>
+    <div class="flex items-center justify-center flex-col" @click="toggleSettings">
+      <SvgIcon icon="ri:settings-line" class="text-3xl"></SvgIcon>
+      <div class="text-[13px]">设置</div>
+    </div>
   </div>
-
-  <n-drawer v-model:show="st.show"  class="!h-[90vh] !max-h-[660px]"     placement="bottom" v-if="goHome=='draw'">
-    <n-drawer-content   style="--n-body-padding:0" class="h-full">
-      <aiDrawInput @draw-sent="drawSent" @close="st.show=false"  />
+  <n-drawer v-model:show="st.show" class="!h-[90vh] !max-h-[660px]" placement="bottom" v-if="goHome == 'draw'">
+    <n-drawer-content style="--n-body-padding:0" class="h-full">
+      <aiDrawInput @draw-sent="drawSent" @close="st.show = false" />
+    </n-drawer-content>
+  </n-drawer>
+  <n-drawer v-model:show="showSettings" class="!h-[90vh] !max-h-[660px]" placement="right">
+    <n-drawer-content style="--n-body-padding:0" class="h-full">
+      <OpenAISettings @close="toggleSettings" />
     </n-drawer-content>
   </n-drawer>
 </template>
